9405541baef0b3dc82f12aaa23214872fccf0345
老土 <2427796523@qq.com>
2022-11-03 17:52:53 +0800

    :heavy_minus_sign: masa is a blazor theme, so remove mvc project.

--- modules/masa-theme/src/Secyud.Abp.AspNetCore.Mvc.UI.Theme.Masa/MasaMvcLayouts.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Secyud.Abp.AspNetCore.Mvc.UI.Theme.Masa;
-
-public static class MasaMvcLayouts
-{
-    public const string SideMenu = "~/Themes/Masa/Layouts/Application/SideMenuLayout.cshtml";
-    public const string TopMenu = "~/Themes/Masa/Layouts/Application/TopMenuLayout.cshtml";
-}
