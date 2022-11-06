25c1b3be0892d670cc1a6eebdaaf875a4ce9e90c
老土 <2427796523@qq.com>
2022-11-06 08:40:56 +0800

    :heavy_plus_sign: feat: add demo

---
+++ modules/masa-theme/test/Secyud.Abp.AspNetCore.Components.Server.MasaTheme.Demo/Menus/MasaThemeDemoMenuContributor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.UI.Navigation;
+
+namespace Secyud.Abp.AspNetCore.Components.Server.MasaTheme.Demo.Menus;
+
+public class MasaThemeDemoMenuContributor : IMenuContributor
+{
+    public Task ConfigureMenuAsync(MenuConfigurationContext context)
+    {
+        if (context.Menu.Name == StandardMenus.Main)
+        {
+            AddMainMenuItems(context);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private void AddMainMenuItems(MenuConfigurationContext context)
+    {
+        var menuItem = new ApplicationMenuItem(MasaThemeDemoMenus.Components.Root, "Components");
+
+        var items = new List<ApplicationMenuItem>()
+        {
+            new(MasaThemeDemoMenus.Components.Alerts, "Alerts", url: "/Components/Alerts"),
+            new(MasaThemeDemoMenus.Components.Badges, "Badges", url: "/Components/Badges"),
+            new(MasaThemeDemoMenus.Components.Borders, "Borders", url: "/Components/Borders"),
+            new(MasaThemeDemoMenus.Components.Breadcrumbs, "Breadcrumbs", url: "/Components/Breadcrumbs"),
+            new(MasaThemeDemoMenus.Components.Buttons, "Buttons", url: "/Components/Buttons"),
+            new(MasaThemeDemoMenus.Components.ButtonGroups, "Button Groups", url: "/Components/ButtonGroups"),
+            new(MasaThemeDemoMenus.Components.Cards, "Cards", url: "/Components/Cards"),
+            new(MasaThemeDemoMenus.Components.Carousel, "Carousel", url: "/Components/Carousel"),
+            new(MasaThemeDemoMenus.Components.Collapse, "Collapse", url: "/Components/Collapse"),
+            new(MasaThemeDemoMenus.Components.Dropdowns, "Dropdowns", url: "/Components/Dropdowns"),
+            new(MasaThemeDemoMenus.Components.DynamicForms, "Dynamic Forms", url: "/Components/DynamicForms"),
+            new(MasaThemeDemoMenus.Components.FormElements, "Form Elements", url: "/Components/FormElements"),
+            new(MasaThemeDemoMenus.Components.Grids, "Grids", url: "/Components/Grids"),
+            new(MasaThemeDemoMenus.Components.ListGroups, "List Groups", url: "/Components/ListGroups"),
+            new(MasaThemeDemoMenus.Components.Modals, "Modals", url: "/Components/Modals"),
+            new(MasaThemeDemoMenus.Components.Navs, "Navs", url: "/Components/Navs"),
+            new(MasaThemeDemoMenus.Components.Navbars, "Navbars", url: "/Components/Navbars"),
+            new(MasaThemeDemoMenus.Components.Paginator, "Paginator", url: "/Components/Paginator"),
+            new(MasaThemeDemoMenus.Components.Popovers, "Popovers", url: "/Components/Popovers"),
+            new(MasaThemeDemoMenus.Components.ProgressBars, "Progress Bars", url: "/Components/ProgressBars"),
+            new(MasaThemeDemoMenus.Components.Tables, "Tables", url: "/Components/Tables"),
+            new(MasaThemeDemoMenus.Components.Tabs, "Tabs", url: "/Components/Tabs"),
+            new(MasaThemeDemoMenus.Components.Tooltips, "Tooltips", url: "/Components/Tooltips")
+        };
+
+        items.OrderBy(x => x.Name)
+            .ToList()
+            .ForEach(x => menuItem.AddItem(x));
+
+        context.Menu.AddItem(menuItem);
+    }
+}
---
+++ modules/masa-theme/test/Secyud.Abp.AspNetCore.Components.Server.MasaTheme.Demo/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+using Serilog;
+using Serilog.Events;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Secyud.Abp.AspNetCore.Components.Server.MasaTheme.Demo;
+
+var assemblyName = typeof(Program).Assembly.GetName().Name;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+#if DEBUG
+    .MinimumLevel.Debug()
+#else
+                .MinimumLevel.Information()
+#endif
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Application", $"{assemblyName}")
+    .WriteTo.Async(c => c.File("Logs/logs.txt"))
+    .WriteTo.Async(c => c.Console())
+    .CreateLogger();
+
+try
+{
+    Log.Information($"Starting {assemblyName}.");
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host
+        .AddAppSettingsSecretsJson()
+        .UseAutofac()
+        .UseSerilog();
+    await builder.AddApplicationAsync<MasaThemeDemoModule>();
+    var app = builder.Build();
+    await app.InitializeApplicationAsync();
+    await app.RunAsync();
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, $"{assemblyName} terminated unexpectedly!");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
