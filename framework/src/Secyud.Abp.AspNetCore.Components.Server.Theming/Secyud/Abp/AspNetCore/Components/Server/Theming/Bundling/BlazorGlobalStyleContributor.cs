70612dbceb87dd3ea5381e44ccd490965942eead
老土 <2427796523@qq.com>
2022-11-07 09:59:01 +0800

    :heavy_plus_sign: feat: add fontawesome and mdi font

--- framework/src/Secyud.Abp.AspNetCore.Components.Server.Theming/Secyud/Abp/AspNetCore/Components/Server/Theming/Bundling/BlazorGlobalStyleContributor.cs
+++ framework/src/Secyud.Abp.AspNetCore.Components.Server.Theming/Secyud/Abp/AspNetCore/Components/Server/Theming/Bundling/BlazorGlobalStyleContributor.cs
@@ -8,5 +8,7 @@ public class BlazorGlobalStyleContributor : BundleContributor
     public override void ConfigureBundle(BundleConfigurationContext context)
     {
         context.Files.AddIfNotContains("/_content/Masa.Blazor/css/masa-blazor.min.css");
+        context.Files.AddIfNotContains("/_content/Secyud.Abp.MasaBlazorUi/libs/font-awesome/css/font-awesome.min.css");
+        context.Files.AddIfNotContains("/_content/Secyud.Abp.MasaBlazorUi/libs/@mdi/font/css/materialdesignicons.min.css");
     }
 }
