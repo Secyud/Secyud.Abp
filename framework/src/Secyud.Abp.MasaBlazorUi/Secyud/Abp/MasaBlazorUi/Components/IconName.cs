17cbfc798a5beb935e89d22fa184d58d31ec86a8
老土 <2427796523@qq.com>
2022-11-09 11:24:41 +0800

    :technologist: add preset icon names

---
+++ framework/src/Secyud.Abp.MasaBlazorUi/Secyud/Abp/MasaBlazorUi/Components/IconName.cs
@@ -0,0 +1,9 @@
+namespace Secyud.Abp.MasaBlazorUi.Components;
+
+public static class IconName
+{
+    public const string Create = "mdi-file-plus";
+    public const string Update = "mdi-table-edit";
+    public const string Delete = "mdi-delete";
+    public const string Search = "mdi-magnify";
+}
